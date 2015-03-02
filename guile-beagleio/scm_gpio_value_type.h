7c2b7d4ddef76bea6a645028b832b6161fea96c0
Chad Albers <calbers@neomantic.com>
2015-03-02 23:02:41 +0900

    removed function that is no longer used

--- guile-beagleio/scm_gpio_value_type.h
+++ guile-beagleio/scm_gpio_value_type.h
@@ -4,7 +4,6 @@
 #include <libguile.h>
 
 void init_gpio_value_type(void);
-void gpio_value_smob_to_bbio_value(SCM *value_smob, int *value);
 SCM scm_new_gpio_value_smob(const int value);
 void scm_assert_gpio_value_smob(SCM *smob);
 
